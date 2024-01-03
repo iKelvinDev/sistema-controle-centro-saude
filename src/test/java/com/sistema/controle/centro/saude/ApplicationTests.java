42833eaf6884aedb7f98af808ac3fb4525cb0934
kelvin <kelvinmarquesk@gmail.com>
2024-01-03 07:27:10 -0300

    First commit

---
+++ src/test/java/com/sistema/controle/centro/saude/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.controle.centro.saude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
